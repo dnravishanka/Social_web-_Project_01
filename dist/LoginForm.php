bbb281da8552a2260c085b65f6d27d1ea29c99ae
dnravishanka <51900228+dnravishanka@users.noreply.github.com>
2019-08-05 10:23:10 +0530

    -commit

--- dist/LoginForm.php
+++ dist/LoginForm.php
@@ -4,4 +4,7 @@
  * User: Nadun
  * Date: 8/5/2019
  * Time: 10:17 AM
- */
+ */?>
+<div><p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab amet beatae cumque deserunt, dignissimos dolor explicabo inventore, minima natus necessitatibus nemo nihil non omnis quis ratione recusandae repudiandae, soluta voluptas.
+    </p></div>
